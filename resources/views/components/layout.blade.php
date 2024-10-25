cea1e1daa5ae91ca0c46277505e6eb134cac2a74
Jean Sacren <sakiwit@gmail.com>
2024-10-24 22:51:18 -0600

    Use custom responsive-nav-link
    Use responsive-nav-link in lieu of anchor tag for mobile, but with
    Tailwind utility updates in sync with desktop implementation.
    
    Retain aria-current and add it to responsive-nav-link.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -76,9 +76,9 @@
         <div class="md:hidden" id="mobile-menu">
           <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="/home" class="{{ request()->is('home') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
-            <a href="/about" class="{{ request()->is('about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium">About</a>
-            <a href="/contact" class="{{ request()->is('contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium">Contact</a>
+            <x-responsive-nav-link href="/home" :active="request()->is('home')">Home</x-responsive-nav-link>
+            <x-responsive-nav-link href="/about" :active="request()->is('about')">About</x-responsive-nav-link>
+            <x-responsive-nav-link href="/contact" :active="request()->is('contact')">Contact</x-responsive-nav-link>
           </div>
           <div class="border-t border-gray-700 pb-3 pt-4">
             <div class="flex items-center px-5">

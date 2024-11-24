c4f10d096a0d57e758ab104090b4e761305bd587
Jean Sacren <sakiwit@gmail.com>
2024-11-24 16:17:26 -0700

    Add format to injected content
    Let the injected content look aligned and nice with the header content.
    
    Fix Home page markup formatting.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,11 @@
 <x-app-layout>
-  <x-slot:title>
-    Home Page
-  </x-slot">
+    <x-slot:title>
+        Home Page
+    </x-slot">
 
-  <x-slot:header>
-    Home Page
-  </x-slot">
+    <x-slot:header>
+        Home Page
+    </x-slot">
+
+    <h1>Hello, world!</h1>
 </x-app-layout>

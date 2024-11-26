2c54dc0f0b870ebd7d16fc867310180f125d4d3c
Jean Sacren <sakiwit@gmail.com>
2024-11-25 21:53:53 -0700

    Add link to individual job
    Build links to individual job in jobs view.
    
    Need to use Tailwind CSS to make views of jobs and job much more
    attractive.

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -7,9 +7,14 @@
         Joblistings
     </x-slot">
 
-    <ul class="list-decimal">
+    <ul class="list-disc">
         @foreach ($jobs as $job)
-        <li>{{ $job['title'] }} - Pays {{ $job['salary'] }} in {{ $job['location'] }}</li>
+        <li>
+            <a href="/jobs/{{ $job['id'] }}" class="hover:underline text-teal-500 font-bold">
+                {{ $job['title'] }}
+            </a>
+            - Pays {{ $job['salary'] }} in {{ $job['location'] }}
+        </li>
         @endforeach
     </ul>
 </x-app-layout>

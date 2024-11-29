6f0ba6b03d1d3a09903b1c2b593c393d00a3e92d
Jean Sacren <sakiwit@gmail.com>
2024-11-28 23:30:16 -0700

    Allow mass assignment on [App\Models\Job]
    By default Job class doesn't allow mass assignment and will throw
    MassAssignmentException.  Add fillable property from scratch with
    [title] and [salary] to allow mass assignment on [App\Models\Job].

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     protected $table = 'job_listings';
+    protected $fillable = [
+        'title',
+        'salary',
+    ];
 }

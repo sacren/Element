cabbc3199bb3ba9036e20adad9db069f8ef06a03
Jean Sacren <sakiwit@gmail.com>
2024-12-02 23:00:32 -0700

    Add employer has many jobs relation
    Inside Employer class, build jobs method that forms employer has many
    jobs relation.

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the jobs owned by the employer.
+     *
+     * @return HasMany relation
+     */
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
 }

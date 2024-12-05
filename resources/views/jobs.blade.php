7f06dcc069a3b8ccb6d838f20238cf442a98fe5e
Jean Sacren <sakiwit@gmail.com>
2024-12-05 14:39:56 -0700

    Add employer to the job listings
    Extract the employer name from the passed-in jobs and add it above the
    job listing.  Use block layout to let employer name and job listing flow
    vertically.  Add color to text and border to spruce up the listings.
    
    The passed-in jobs are a collection.  Use API to manage the attributes.

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -7,11 +7,16 @@
         Job Listings
     </x-slot">
 
-    <ul class="list-disc">
+    <ul class="list-none space-y-6">
         @foreach ($jobs as $job)
         <li>
-            <a href="/jobs/{{ $job['id'] }}" class="hover:underline text-teal-500 font-bold">
-                {{ $job['title'] }}
+            <a href="/jobs/{{ $job->id }}" class="block px-4 py-4 border-2 border-red-300">
+                <div class="text-purple-500">
+                    {{ $job->employer->name }}
+                </div>
+                <div class="text-teal-500 font-bold">
+                    {{ $job->title }}
+                </div>
             </a>
         </li>
         @endforeach

5233927b4f9e9c412fe61c20ef232df877f8edbb
Jean Sacren <sakiwit@gmail.com>
2024-12-05 20:24:06 -0700

    Prevent all models from lazy loading
    After preventing Job model from lazy loading, now we move forward to
    prevent all models from lazy loading.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Job;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Job::preventLazyLoading();
+        Model::preventLazyLoading();
     }
 }

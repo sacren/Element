2e563053d74d232d18b2dcdb864f381383237696
Jean Sacren <sakiwit@gmail.com>
2024-12-06 21:20:01 -0700

    Let default seeder include other seeders
    With other seeders, the default seeder makes migrate:fresh --seed very
    convenient and powerful while individual seeders can be run separately.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,22 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Seed users and other database tables.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(JobSeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(JobTagSeeder::class);
     }
 }

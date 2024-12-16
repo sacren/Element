a8c7165c57d793d07712bc8bbfa28d90959d5381
Jean Sacren <sakiwit@gmail.com>
2024-12-15 23:44:18 -0700

    Use employerUser->id

--- app/Policies/JobPolicy.php
+++ app/Policies/JobPolicy.php
@@ -36,7 +36,7 @@ class JobPolicy
      */
     public function update(User $user, Job $job): bool
     {
-        return $user->id === $job->employer_id;
+        return $user->id === $job->employerUser->id;
     }
 
     /**
@@ -44,7 +44,7 @@ class JobPolicy
      */
     public function delete(User $user, Job $job): bool
     {
-        return $user->id === $job->employer_id;
+        return $user->id === $job->employerUser->id;
     }
 
     /**

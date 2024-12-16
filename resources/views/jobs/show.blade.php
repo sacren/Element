356b121f0b1b9a15336c9ca3875b83cd94bc6256
Jean Sacren <sakiwit@gmail.com>
2024-12-15 23:45:25 -0700

    Use @can directive for update and delete methods
    Apply policy in show view through the use of @can directive.

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -10,18 +10,22 @@
     <p>The Pay is {{ $job->salary }} per year.</p>
 
     <div class="mt-4 flex gap-4">
-        <x-button-link href="{{ route('jobs.edit', $job->id) }}">
-            Edit Job
-        </x-button-link>
+        @can('update', $job)
+            <x-button-link href="{{ route('jobs.edit', $job->id) }}">
+                Edit Job
+            </x-button-link>
+        @endcan
 
-        <form method="POST" action="{{ route('jobs.destroy', $job->id) }}">
-            @csrf
-            @method('DELETE')
-            <button
-                type="submit"
-                class="rounded-md bg-red-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
-                Delete Job
-            </button>
-        </form>
+        @can('delete', $job)
+            <form method="POST" action="{{ route('jobs.destroy', $job->id) }}">
+                @csrf
+                @method('DELETE')
+                <button
+                    type="submit"
+                    class="rounded-md bg-red-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
+                    Delete Job
+                </button>
+            </form>
+        @endcan
     </div>
 </x-app-layout>

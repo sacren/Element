0dbe4c7feb8c4836db759aa010d37e89eca8f1b1
Jean Sacren <sakiwit@gmail.com>
2024-12-16 22:21:16 -0700

    Add auth service provider
    Build AuthServiceProvider scaffolding using make:provider tool.
    
    $ sail artisan make:provider -- AuthServiceProvider
    
       INFO  Provider [app/Providers/AuthServiceProvider.php] created successfully.
    
    Build the policies property of [Job::class => JobPolicy::class] array.
    
    Laravel uses convention over configuration.  Without the above manual
    interaction, Laravel will implement it implicitly through the use of
    AuthServiceProvider default name.  With the above, it merely makes the
    process explicit.
    
    The make:provider command automatically updates bootstrap/providers.php
    with AuthServiceProvider class.

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Job;
+use App\Policies\JobPolicy;
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        Job::class => JobPolicy::class,
+    ];
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}

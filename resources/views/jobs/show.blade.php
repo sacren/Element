5934de2462792af512646fa3c1efd4f6cdb4736a
Jean Sacren <sakiwit@gmail.com>
2024-12-17 20:01:38 -0700

    Revert "Use @can directive for update and delete methods"
    This reverts commit 356b121f0b1b9a15336c9ca3875b83cd94bc6256.
    
    By enforcing JobPolicy, @can directive is not necessary.  If the current
    user is not authorized, they will be denied to edit or delete.

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -10,22 +10,18 @@
     <p>The Pay is {{ $job->salary }} per year.</p>
 
     <div class="mt-4 flex gap-4">
-        @can('update', $job)
-            <x-button-link href="{{ route('jobs.edit', $job->id) }}">
-                Edit Job
-            </x-button-link>
-        @endcan
+        <x-button-link href="{{ route('jobs.edit', $job->id) }}">
+            Edit Job
+        </x-button-link>
 
-        @can('delete', $job)
-            <form method="POST" action="{{ route('jobs.destroy', $job->id) }}">
-                @csrf
-                @method('DELETE')
-                <button
-                    type="submit"
-                    class="rounded-md bg-red-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
-                    Delete Job
-                </button>
-            </form>
-        @endcan
+        <form method="POST" action="{{ route('jobs.destroy', $job->id) }}">
+            @csrf
+            @method('DELETE')
+            <button
+                type="submit"
+                class="rounded-md bg-red-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
+                Delete Job
+            </button>
+        </form>
     </div>
 </x-app-layout>
